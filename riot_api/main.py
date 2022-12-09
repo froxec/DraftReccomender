d72a72aa462dd3f68bc59ad4df78d95b20d8872c
froxec <picmego@gmail.com>
2022-12-09 21:04:32 +0100

    constructing matches dataset

--- riot_api/main.py
+++ riot_api/main.py
@@ -11,6 +11,7 @@ def display_player_info(player_name, region):
     print("{name} is good with:".format(name=player.name))
     print([cm.champion.name for cm in great_with])
 
+
 def get_match_participants_features(match):
     participants_features = {}
     for i, participant in enumerate(match.participants):
@@ -22,11 +23,51 @@ def get_player_history(player_name, region):
     player = cass.Summoner(name=player_name, region=region)
     return player.match_history
 
+def current_match_to_history_index(current_match, match_history):
+    for match_num, match in enumerate(match_history):
+        if current_match.id == match.id:
+            return match_num
+    
+def construct_matches_dataset():
+    ids_in_dataset = []
+    players_in_dataset = {}
+    players_ids = []
+    matches = {}
+    challanger = cass.get_challenger_league(queue=cass.data.Queue("RANKED_SOLO_5x5"), region="EUW")
+    for entry in challanger.entries[0:1]:
+        player = entry.summoner
+        player_match_history = player.match_history
+        if player.puuid in players_ids:
+            print("Summoner {} already in dataset".format(player.name))
+            continue
+        for i, match in enumerate(player_match_history):
+            if match.id in ids_in_dataset:
+                continue
+            else:
+                match_index = len(matches)
+                matches[str(match_index)] = {}
+                for user_num, participant in enumerate(match.participants):
+                    summoner = participant.summoner
+                    if summoner.puuid not in players_ids:
+                        summoner_match_history = [match for match in summoner.match_history]
+                        # print("Adding summoner {} to dataset".format(summoner.name))
+                        # print("Match {current_match} is {match_num} in {summ_name} history".format(current_match=match.id,
+                        #                                                                            match_num=current_match_to_history_index(match, summoner.match_history),
+                        #                                                                            summ_name=summoner.name))
+                        players_ids.append(summoner.puuid)
+                        players_in_dataset[summoner.puuid] = {"id": len(players_in_dataset), "match_history": summoner_match_history}
+                    matches[str(match_index)]["User" + str(user_num)] = (players_in_dataset[summoner.puuid]["id"], current_match_to_history_index(match, players_in_dataset[summoner.puuid]["match_history"]))
+                #matches[str(match_index)] = {"id" : match.id, "match": match}
+                ids_in_dataset.append(match.id)
+    return matches
+
 def getAPI_key():
     file = open("API_key.txt")
     return file.read()
 cass.set_riot_api_key("RGAPI-dc1b2169-925a-4b22-99eb-4ea6b6e2d8dd")
 
-player_history = get_player_history("MDP Froxec", "EUW")
-match = player_history[0]
-print(get_match_participants_features(match)['User4'])
+# player_history = get_player_history("MDP Froxec", "EUW")
+# match = player_history[0]
+# print(get_match_participants_features(match)['User4'])
+matches_dataset = construct_matches_dataset()
+print(matches_dataset)
